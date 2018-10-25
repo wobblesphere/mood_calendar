bee567e4f6985cef24e1d55c5f4d0f417e42f60a
jyh106 <jyh106@berkeley.edu>
2018-10-24 20:28:09 -0700

    getting data from server

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import CalendarBoard from './CalendarBoard/CalendarBoard.js';
 import PopUpForm from './PopUpForm/PopUpForm.js';
 import CalendarLegend from "./CalendarLegend/CalendarLegend.js";
 import { connect } from 'react-redux'
+import axios from 'axios';
+import Actions from './actions/actions.js';
 
 
 class App extends Component {
@@ -29,7 +31,34 @@ class App extends Component {
       </div>
     );
   }
+
+  updateMood(response){
+    this.props.update_Mood({
+      mood: response.data.mood, 
+      squareID: response.data.squareID, 
+      note: response.data.note
+    });
+  }
+
+  getSubmittedMood() {
+    //get from server
+    const getDataRequest = axios.get('http://127.0.0.1:5000/get_mood');
+    getDataRequest
+      .then((response) => {
+        this.updateMood(response)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+  componentDidMount(){
+    this.getSubmittedMood();
+  }
+
 }
+
 
 function mapStateToProps(state) {
   return {
@@ -37,7 +66,16 @@ function mapStateToProps(state) {
   };
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    update_Mood: ({mood, squareID, note}) => {
+      dispatch(Actions.update_Mood({mood, squareID, note}))
+    },
+  }
+}
+
 
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(App)
